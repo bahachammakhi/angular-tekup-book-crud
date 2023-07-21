8c52f0edcb7501515340183bdf572aa1c2aa5e92
Baha eddine chammakhi <baha@vikonnekt.com>
2023-07-22 00:52:29 +0100

    feat: add component

---
+++ src/app/add/add.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Book } from '../models/book';
+import { BookService } from '../services/book.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css'],
+})
+export class AddComponent {
+  constructor(private BookService: BookService, private router: Router) {}
+
+  create(book: Book) {
+    const newBook = {
+      ...book,
+      id: Number(book.id),
+    };
+    this.BookService.addOne(newBook);
+    this.router.navigate(['/list']);
+  }
+}
